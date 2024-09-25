18851485453fa4299daab97c824c235ef76166d0
AWildErin <sharpian126@gmail.com>
2024-09-25 22:09:57 +0100

    Add Editor, Engine, GameInstance and LocalPlayer subsystem templates

---
+++ Classes/Subsystems/ULocalPlayerSubsystem.cpp
@@ -0,0 +1,11 @@
+#include "@{TPL_CLASS_RELPATH}@{TPL_CLASS_NAME}.h"
+
+void U@{TPL_CLASS_NAME}::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void U@{TPL_CLASS_NAME}::Deinitialize()
+{
+	Super::Deinitialize();
+}
